288d3040d5b0e94a4e0f9ad4d67c978909ff7544
s152100 <56384332+s152100@users.noreply.github.com>
2019-10-15 14:31:17 +0200

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -42,18 +42,27 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
             raise ValueError("The number of samples differs between X and y")
 
         # Get all the unique classes
-        classes, indices = np.unique(y, return_inverse=True)
+        self.__classes, indices = np.unique(y, return_inverse=True)
 
         # Probability of belonging to a given class
-        self.__p_y = np.zeros(len(classes))
+        self.__p_y = np.zeros(len(self.__classes))
 
         # Mean of the elements for all the classes
-        self.__moy = np.zeros(len(classes),1)
+        self.__moy = []
 
         # Variance of the elements for all the classes
-        self.__var = np.zeros(len(classes),1)
+        self.__var = []
+        
+        # Matrix of sorted samples in the different classes
+        attributes = []
+        
+        # Creation of the sublists corresponding to the different classes
+        for i in range(len(self.__classes)):
+            self.__moy.append([])
+            self.__var.append([])
+            attributes.append([])
 
-        # Computing __p_y
+        # Computing P(y)
         for i in indices:
             self.__p_y[i] += 1
 
@@ -62,20 +71,14 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
         for i in range(len(self.__p_y)):
             self.__p_y[i] /= div
 
-        # Computing __moy
-
-        #Attention à tester si attributes grandis bien en Y
-        #normalement ca devrait aller chopper tout les éléments de X et les placer dans la colonne correspondante à la classe!! à tester!!!
-        attributes = np.empty([len(classes)])
-        for i in range(len(indices)):
-            attributes[indices[i]][:][i].append(X[i])
-            attributes[indices[i]][:][i].append(X[i])
-
-        #on regarde par rapport à toutes les classes et on prend la moyenne des attibuts de chaque classe
-        for i in classes:
-            self.__moy = np.mean(attributes, axis=2) 
-        #mtn ca devrait etre bon, normalement chaque élément de moy va correspondre à la moyenne des éléments de X en fonction de la classe!! à tester et vérifier!!!
-
+        # Filling attributes with the samples
+        for i in range(len(indices))
+            attributes[indices[i]].append(X[i])
+            
+        # Computation of the mean and the variance
+        for i in range(len(attributes)):
+            self.__moy[i] = np.mean(attributes[i], axis = 0)
+            self.__var[i] = np.var(attributes[i], axis = 0)
 
         return self
 
@@ -92,29 +95,25 @@ class GaussianNaiveBayes(BaseEstimator, ClassifierMixin):
         y : array of shape = [n_samples]
             The predicted classes, or the predict values.
         """
-        for h in range(len(X[:][0])
-            # Computation of the probabilities Pr(xi|y)
-            Pr = np.zeros(len(classes), len(X[0][:]))
-            Pr_classes = np.zeros(len(classes))
-            for i in classes
-                for j in range(len(X[0][:]))
-                    exp_num = math.pow(X[0][j]-self.__moy[i][j], 2)
+        
+        y = []
+        for h in range(len(X)):             # Number of loops = number of samples
+            max = 0
+            for i in range(len(self.__classes)):
+                Py = self.__p_y[i]          # Product of the different probabilities for one class
+                for j in range(len(X[0]):   # Number of loops = number of features
+                    exp_num = math.pow(X[h][j]-self.__moy[i][j], 2)
                     exp_den = 2*self.__var[i][j]
                     exp = math.exp(-exp_num/exp_den)
                     factor_den = math.pow(2*math.pi*self.__var[i][j], 1/2)
-                    Pr[i][j] = (1/factor_den)*exp
+                    Py *= (1/factor_den)*exp
+                if Py > max:
+                   predict_classe = self.__classes[i]
+                   max = Py
+            y[h] = predict_classe
 
-                P = Pr[i][0]*Pr[i][1]
-                Pr_classes[i] = self.__p_y[i]*P
-
-            # Prediction of the classe
-            index = Pr_classes.argmax()
-            y[h] = Pr_classes[index]
-        
         return y
                 
-
-        pass
 
     def predict_proba(self, X):
         """Return probability estimates for the test data X.
